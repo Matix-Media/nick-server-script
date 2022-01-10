8edf38406aeb226bbfaab3974a4db817681d644b
Max Heilmann <support@matix-media.net>
2022-01-10 19:31:54 +0100

    Added server managmant

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,38 @@ const rootDir: string = process.env.ROOT_DIR;
 const webhookLink: string = process.env.WEBHOOK_LINK;
 const serverPort: number = Number.parseInt(process.env.SERVER_PORT);
 
-(async () => {
-    const url = await ngrok.connect({proto: "tcp", addr: serverPort});
+function startMinecraftServer(onClose = (code: number) => {}): Promise<void> {
+    return new Promise<void>((resolve) => {
+        const sp = spawn("start.bat", {cwd: rootDir});
+        sp.stdout.on("data", (buffer: Buffer) => {
+            const line = buffer.toString("utf-8");
 
-    console.log("Ngrok IP:", url);
+            console.log(line);
+
+            if (!line.includes("[Server thread/INFO]: Done") && !line.includes("! For help, type \"help\"")) return;
+            resolve();
+        });
+
+        sp.on("close", (code) => {
+            onClose(code);
+        })
+    });
+}
+
+(async () => {
+    let url: string;
+    
+    console.log("Starting Minecraft server...");
+    await startMinecraftServer(async (code) => {
+        console.error("Minecraft server stopped with code", code);
+        await ngrok.disconnect(url);
+    });
+    console.log("Server started successfully");
+
+    url = await ngrok.connect({proto: "tcp", addr: serverPort});
+
+    console.log("Server reachable through:", url);
+    
     axios.post(webhookLink, {
         embeds: [{
             title: "Neue Minecraft Server Adresse",
@@ -24,5 +52,7 @@ const serverPort: number = Number.parseInt(process.env.SERVER_PORT);
             }
         }]
     });
+
+    
 })();
 
