04a34d826e9ad68886a7829851447492a2a7cd47
Max Heilmann <support@matix-media.net>
2022-01-10 19:11:35 +0100

    maybe

--- src/index.ts
+++ src/index.ts
@@ -2,72 +2,27 @@ import {config} from "dotenv";
 import {spawn} from "child_process";
 import axios from "axios";
 import * as path from "path";
+import * as ngrok from "ngrok";
 
 config();
 
 const rootDir: string = process.env.ROOT_DIR;
-const ngrokCommand: string = process.env.NGROK_COMMAND;
-const ngrokTimeout: number = Number.parseInt(process.env.NGROK_TIMEOUT);
 const webhookLink: string = process.env.WEBHOOK_LINK;
+const serverPort: number = Number.parseInt(process.env.SERVER_PORT);
 
-let ngrokStarted = false;
-let ls;
+(async () => {
+    const url = await ngrok.connect({proto: "tcp", addr: serverPort});
 
-function killNgrok() {
+    console.log("Ngrok IP:", url);
+    axios.post(webhookLink, {
+        embeds: [{
+            title: "Neue Minecraft Server Adresse",
+            description: "Der Minecraft Server hat eine neue Adresse zugewiesen bekommen.\n\n**IP**: `" + url + "`",
+            footer: {
+                text: "Offizieller Freudenhaus SMP Server HD LP xX Bot",
+                icon_url: "https://cdn.discordapp.com/icons/778965930746183691/1585148368fa55f3eff29557bc23d640.webp?size=32"
+            }
+        }]
+    });
+})();
 
-    try {
-        ls.kill();
-    } catch (_) {}
-}
-
-const ngrokTimeoutWorker = setTimeout(() => {
-    if (ngrokStarted) return;
-
-    console.error("Ngrok not started within timeout of", ngrokTimeout, "milliseconds! Closing.");
-    killNgrok();
-}, ngrokTimeout);
-
-function ngrokOutputParse(bLine: Buffer) {
-    const line = bLine.toString("utf-8");
-
-    console.log(line);
-
-    if (line.includes("Your account is limited to")) {
-        console.error("Ngrok session limit reached! Closing.")
-        clearTimeout(ngrokTimeoutWorker);
-        killNgrok();
-        return;
-    }
-
-    if (!line.includes("Forwarding")) return;
-
-    for (let part of line.split(" ")) {
-        part = part.trim();
-        if (part == "" || part == null) continue;
-        if (!part.includes("ngrok.io")) continue;
-
-        ngrokStarted = true;
-        console.log("Ngrok IP:", part);
-        axios.post(webhookLink, {
-            embeds: [{
-                title: "Neue Minecraft Server Adresse",
-                description: "Der Minecraft Server hat eine neue Adresse zugewiesen bekommen.\n\n**IP**: `" + part + "`",
-                footer: {
-                    text: "Offizieller Freudenhaus SMP Server HD LP xX Bot",
-                    icon_url: "https://cdn.discordapp.com/icons/778965930746183691/1585148368fa55f3eff29557bc23d640.webp?size=32"
-                }
-            }]
-        });
-    }
-}
-
-console.log(`Starting ngrok in "${rootDir}" with command "${ngrokCommand.split(" ")}"`);
-ls = spawn("ngrok", ngrokCommand.split(" "), {cwd: rootDir});
-ls.stdout.on("data", ngrokOutputParse);
-ls.stderr.on("data", ngrokOutputParse);
-ls.on("error", (error) => {
-    console.error("An error occurred while starting ngrok proxy:", error)
-});
-ls.on("close", code => {
-    console.warn("Ngrok closed with code:", code);
-});
